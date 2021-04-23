bb0b5d4df6f1b3ce5015e0b4f2c74a4281b4808b
Amine <aminebarbouch@gmail.com>
2021-04-23 15:41:44 +0200

    Ajout de requêtes (qui ne veuelent pas marcher)

--- client/src/js/scripts.js
+++ client/src/js/scripts.js
@@ -1,5 +1,26 @@
 const handleTrouverGenre = (e) => {
+  //e.preventDefault();
   console.log("fonction lancée!");
-  
+
+  axios({
+    method: "get",
+    url: `http://0.0.0.0:3000/jeux`,
+  })
+    .then((res) => {
+      //console.log(res);
+      if (res.data.errors) {
+        var d = document.createElement("div");
+        d.innerHTML = "error";
+      } else {
+        // window.location = "/";
+        console.log("trouvé");
+        var d = document.createElement("div");
+        d.innerHTML = "trouvé";
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    
   return false;
 };
